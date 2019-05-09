8c6766ca4d0b2b1b36a87e4e8696de96d8e9297a
Chandan Kapilavai <ckapilavai@altimetrik.com>
2019-05-09 12:43:54 +0530

    removed unneccessary dependencies

--- src/main/java/com/nuestar/neustarcatserv/NuestarCategoryApplication.java
+++ src/main/java/com/nuestar/neustarcatserv/NuestarCategoryApplication.java
@@ -2,9 +2,8 @@ package com.nuestar.neustarcatserv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class) 
+@SpringBootApplication 
 public class NuestarCategoryApplication {
 
 	public static void main(String[] args) {
